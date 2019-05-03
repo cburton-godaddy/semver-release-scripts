19534794e3d8c5d600b60f08bce85c2a3fc9d837
Connor Burton <Connor.burton@warwickicsystems.com>
2019-05-03 11:24:15 +0100

    Fixing issue with migrations not running correctly as promises were not returning correctly

--- src/folders.js
+++ src/folders.js
@@ -36,24 +36,24 @@ class Folders {
      * @returns {Promise}
      */
     filterFilenames(filenames) {
-        return new Promise((resolve, reject) => {
-            const folders = [];
-            filenames.forEach((file, index) => {
+        const promises = filenames.map((file, index) => {
+            return new Promise((resolve, reject) => {
                 fs.stat(path.resolve(this.source, file), (err, stats) => {
-                    if (stats.isDirectory()) {
-                        if (semver.valid(file)) {
-                            folders.push(file);
-                        } else if (this.verbose) {
+                    if (!semver.valid(file) || !stats.isDirectory()) {
+                        if (this.verbose) {
                             console.info(`Ignoring ${this.source}${path.sep}${file} as it is not a valid semver`);
                         }
+
+                        resolve();
+                        return;
                     }
 
-                    if (index === filenames.length - 1) {
-                        resolve(folders);
-                    }
+                    resolve(file);
                 });
             });
         });
+
+        return Promise.all(promises);
     }
 
     /**
@@ -62,22 +62,24 @@ class Folders {
      * @returns {Promise}
      */
     filterDirectoriesWithIndex(directories) {
-        return new Promise((resolve, reject) => {
-            const folders = [];
-            directories.forEach((dir, index) => {
+        const promises = directories.filter(Boolean).map((dir, index) => {
+            return new Promise((resolve, reject) => {
                 fs.access(path.resolve(this.source, dir, 'index.js'), fs.constants.R_OK, err => {
-                    if (err && this.verbose) {
-                        console.info(`Ignoring ${this.source}${path.sep}${dir} as an index.js does not exist`);
-                    } else {
-                        folders.push(dir);
+                    if (err) {
+                        if (this.verbose) {
+                            console.info(`Ignoring ${this.source}${path.sep}${dir} as an index.js does not exist`);
+                        }
+
+                        resolve();
+                        return;
                     }
-                    
-                    if (index === directories.length - 1) {
-                        resolve(folders);
-                    }
+
+                    resolve(dir);
                 });
             });
         });
+
+        return Promise.all(promises);
     }
 
     /**
